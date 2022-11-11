4e085ec8c2718c55809789f4323d5b7c3dc44d3c
Mulu <brianmulu007@gmail.com>
2022-11-11 13:02:06 +0300

    Editing document title

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import About from "./components/About";
 import Addbook from "./components/Addbook";
 import Collection from "./components/Collection";
@@ -6,6 +7,10 @@ import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
 function App() {
+  useEffect(() => {
+    document.title = "BOOK SPOT";
+  }, []);
+  
   return (
     <div>
       <Navbar/>
