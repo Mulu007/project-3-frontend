3825032a04a0d2aecd64100858e7b7adc50d9955
Mulu <brianmulu007@gmail.com>
2022-11-11 23:11:30 +0300

    Removal of search bar

--- src/components/Collection.jsx
+++ src/components/Collection.jsx
@@ -33,10 +33,6 @@ const Collection = () => {
       <div className="text-4xl text-center font-bold py-4">
         <h1>Our Collection</h1>
       </div>
-      <div className='bg-gray-100 sm:ml-60 md:ml-96 rounded-full flex justify-center items-center px-2 w-[200px] sm:w-[400px] lg:w-[500px]'>
-                <AiOutlineSearch size={30}/>
-                <input className="bg-transparent p-2 focus:outline-none w-full" type="text" placeholder='Search books' />
-            </div>
       <div className="container-mx-auto ">
         
         <div className="grid grid-cols-2 lg:grid-cols-4 gap-6 pt-4">
