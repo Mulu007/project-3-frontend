7b6dad734dcf0f1ef9e13d28282a329a51bdc7eb
Mulu <brianmulu007@gmail.com>
2022-11-11 11:00:02 +0300

    Creation of collection section

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import About from "./components/About";
+import Collection from "./components/Collection";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
@@ -8,6 +9,7 @@ function App() {
       <Navbar/>
       <Hero/>
       <About/>
+      <Collection/>
     </div>
   );
 }
